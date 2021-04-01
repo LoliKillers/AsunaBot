3fd4422325ad3c5fec0dc0061ccf680cdd1c6aeb
002 <80396688+Zer0-Tw0@users.noreply.github.com>
2021-04-01 16:18:21 +0700

    config

---
+++ sample_config.py
@@ -0,0 +1,15 @@
+BOT_TOKEN = "1455:AAE4qFS3nF-0Tn" # Get one from @BotFather
+API_ID = 69 # Get variable from my.telegram.org
+API_HASH = "e46b6c854d2bf580" # Get variable from my.telegram.org
+OWNER_ID = 1243703097 # Enter id not username
+MAIN_CHATS = [-1001485393652]  # NOTE These are the chats where main functions of bot will work, like music downloading etc.
+SUDO_USER_ID = [51635132416, 1342121354245, 23452465243]
+SUDO_CHAT_ID = [354165569, 564426514, 686215463]
+LOG_GROUP_ID = -1001337393587
+FERNET_ENCRYPTION_KEY = "iKMq0WZMnJKjMQxZWKtv-cplMuF_LoyshXj0XbTGGWM="
+WELCOME_DELAY_KICK_SEC = 300
+MONGO_DB_URI = "mongodb+srv://username:password@cluster0.ksiis.mongodb.net/YourDataBaseName?retryWrites=true&w=majority"
+
+
+# NOTE Don't make changes below this line
+ARQ_API_BASE_URL = "https://thearq.tech"
